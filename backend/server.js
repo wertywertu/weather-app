28409f08c79845f05e9358d1a0b347463c442431
wertywertu <wertywertu2@gmail.com>
2026-01-20 13:29:25 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,34 @@
 import express from "express";
-import fetch from "node-fetch";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 app.use(cors({ origin: "*" }));
 
 const API_KEY = process.env.OPENWEATHER_API_KEY;
-console.log("OPENWEATHER_API_KEY =", API_KEY);
 
 app.get("/weather", async (req, res) => {
   const city = req.query.city;
-  const apiKey = process.env.API_KEY;
+
+  if (!city) {
+    return res.status(400).json({ error: "City is required" });
+  }
 
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+        city
+      )}&units=metric&appid=${API_KEY}`
     );
+
     const data = await response.json();
     res.json(data);
-  } catch (err) {
+
+  } catch (error) {
     res.status(500).json({ error: "Server error" });
   }
 });
 
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () =>
-  console.log(`Server running on ${PORT}`)
+  console.log("Server running on", PORT)
 );
