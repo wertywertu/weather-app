e4fb9a395d0f05bde1343e5cdaa4a443bd698ef6
wertywertu <wertywertu2@gmail.com>
2026-01-20 13:06:19 +0200

    Backend commit

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,8 @@ app.get("/weather", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on http://localhost:5000");
-});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on ${PORT}`)
+);
